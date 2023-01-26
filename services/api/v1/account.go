783706e6c842b84455306ae48dc7956cdc6713ca
bagashiz <work.bitsy@simplelogin.com>
2023-01-26 11:47:47 +0700

    feat: add more account API services

--- services/api/v1/account.go
+++ services/api/v1/account.go
@@ -34,6 +34,17 @@ func GetAccount(ctx context.Context, id uint) (models.Account, error) {
 	return account, err
 }
 
+// getAccountForUpdate is a function to get an account for a user by account ID for update using FOR NO KEY UPDATE feature.
+func getAccountForUpdate(ctx context.Context, id uint) (models.Account, error) {
+	var account models.Account
+	err := db.GetDB().
+		Set("gorm:query_option", "FOR NO KEY UPDATE").
+		First(&account, id).
+		Error
+
+	return account, err
+}
+
 // ListAccountsParams is a struct to hold the parameters for listing accounts for a user with pagination.
 type ListAccountsParams struct {
 	Limit  int `json:"limit"`
@@ -46,4 +57,30 @@ func ListAccounts(ctx context.Context, arg ListAccountsParams) ([]models.Account
 	err := db.GetDB().Limit(arg.Limit).Offset(arg.Offset).Find(&accounts).Error
 
 	return accounts, err
+}
+
+// DeleteAccount is a function to delete an account for a user by account ID.
+func DeleteAccount(ctx context.Context, id uint) error {
+	err := db.GetDB().Delete(&models.Account{}, id).Error
+
+	return err
+}
+
+// UpdateAccountBalanceParams is a struct to hold the parameters for changing the balance of an account.
+type UpdateAccountBalanceParams struct {
+	ID     uint  `json:"id"`
+	Amount int64 `json:"amount"`
+}
+
+// UpdateAccountBalance is a function to change the balance of an account.
+func UpdateAccountBalance(ctx context.Context, arg UpdateAccountBalanceParams) (models.Account, error) {
+	var account models.Account
+	var updatedBalance int64
+
+	account, _ = getAccountForUpdate(ctx, arg.ID)
+	updatedBalance = account.Balance + arg.Amount
+
+	err := db.GetDB().Model(&account).Update("balance", updatedBalance).Error
+
+	return account, err
 }
